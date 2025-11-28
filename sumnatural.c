08d42a6ab882cc421749a00dd4adc0c6e4afe768
56l2preethikapoor-ai <56l2preethikapoor@gmail.com>
2025-11-28 03:01:50 -0500

    Add files via upload

---
+++ sumnatural.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,s;
+	printf("enter n");
+	scanf("%d",&n);
+	s=n*(n+1)/2;
+	printf("sum of n natural numbers are %d",s);
+	getch();
+	
+}
