29c947e95f26c1b9df214735c1aa4b048943197b
56l2preethikapoor-ai <56l2preethikapoor@gmail.com>
2025-11-28 02:51:57 -0500

    Add files via upload

---
+++ circumference.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int r; float s;
+	printf("enter r");
+	scanf("%d",&r);
+	s=2*3.14*r;
+	printf("circumference of the circle is %f",s);
+	getch();
+}
