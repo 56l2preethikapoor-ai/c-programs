10a20f448247ab2eaf4643ba397e01ead94786c4
56l2preethikapoor-ai <56l2preethikapoor@gmail.com>
2025-11-28 02:53:26 -0500

    Add files via upload

---
+++ perimeterrectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int l,b,c;
+	printf("enter the values of l and b");
+	scanf("%d%d",&l,&b);
+	c=2*(l+b);
+	printf("perimeter of rectangle is %d",l,b);
+	getch();
+}
