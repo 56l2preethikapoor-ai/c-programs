e9ad3ad37a1be2ca412bb6d2f8f85844b75a5adf
56l2preethikapoor-ai <56l2preethikapoor@gmail.com>
2025-11-28 02:53:02 -0500

    Add files via upload

---
+++ rectanglearea.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int l,b,c;
+	printf(" entyer the values of l and b");
+	scanf("%d%d",&l,&b);
+	c=l*b;
+	printf("area of rectangle is %d",c);
+	getch();
+	
+}
