375575bfd7ce9de756f06f49a2fa193d021c51a8
56l2preethikapoor-ai <56l2preethikapoor@gmail.com>
2025-12-18 02:36:26 -0500

    Add files via upload

---
+++ biggestof2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	printf("enter the values of a and b");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	printf("a is max");
+	else
+	printf("b is max");
+	getch();
+}
