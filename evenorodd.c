108ff7f6a4a69c25223bb4a4b203e81993e393aa
56l2preethikapoor-ai <56l2preethikapoor@gmail.com>
2025-12-18 02:36:02 -0500

    Add files via upload

---
+++ evenorodd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a;
+	printf("enter the value of a");
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf(" a is even");
+	}
+	else
+	{
+		printf("b is odd");
+	}
+	getch();
+}
